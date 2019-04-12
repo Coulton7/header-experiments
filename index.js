4c7876dfc0c54ff1ccc587e715b505a0e5462fca
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-04-12 13:38:25 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+(function ($){
+$('.js-slick-slider').each(function() {
+  const $this = $(this);
+  
+  $this.slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,   // <!-- *
+    autoplay: true,
+    autoplaySpeed: 7500,
+    pauseOnHover: true,
+    dots: false,
+    arrows: false,
+  });
+  $(this).find('.start-slide').removeClass('start-slide'); 
+});
+})(jQuery);
+
+ $('a[data-slide]').click(function(e) {
+   e.preventDefault();
+   var slideno = $(this).data('slide');
+   $('.slick-slider').slick('slickGoTo', slideno - 1);
+ });
+
+
+$('.js-slick-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
+    $('li:nth-child('+(currentSlide - 1)+')').removeClass('active');
+  $('ul li:nth-child(' + (currentSlide)+ ')').addClass('active');
+});
+
+$('.js-slick-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
+  if(currentSlide === 0){
+    $('ul li:nth-child(4)').removeClass('active');
+  }
+});
+
+ $('a.previous').click(function(e) {
+$('.js-slick-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
+    $('li:nth-child('+(currentSlide + 1)+')').removeClass('active');
+  $('ul li:nth-child(' + (currentSlide)+')').addClass('active');
+});
+});
+
