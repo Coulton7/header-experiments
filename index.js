2354429c31def53d16a77b54dbb34822d11377cd
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-04-15 14:18:27 +0100

    Wrong placement of a . to define class throws the script off

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ $(".js-slick-slider").on("beforeChange", function(event, slick, currentSlide, ne
 $('.menu li').on('mouseenter', '.js-slick-slider', function(e){
   var $currTarget = $(e.currTarget),
   index = $currTarget.data('slide'),
-  slickObj = $.('.slick-slider').slick('slickGoTo', index + 1)
+  slickObj = $('.slick-slider').slick('slickGoTo', index + 1)
 });
 
 
