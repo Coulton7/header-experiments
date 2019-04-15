a6e2cb908b28c8cac67339fcfb4b5ead62e5cb5d
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-04-15 10:26:09 +0100

    beautified the js to tidy it up a bit better

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
-(function ($){
-$('.js-slick-slider').each(function() {
-  const $this = $(this);
+(function($) {
+  $('.js-slick-slider').each(function() {
+    const $this = $(this);
 
-  $this.slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 7500,
-    pauseOnHover: true,
-    dots: false,
-    arrows: false,
+    $this.slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 7500,
+      pauseOnHover: true,
+      dots: false,
+      arrows: false,
+    });
+    $(this).find('.start-slide').removeClass('start-slide');
   });
-  $(this).find('.start-slide').removeClass('start-slide');
-});
 })(jQuery);
 
- $('a[data-slide]').click(function(e) {
-   e.preventDefault();
-   var slideno = $(this).data('slide');
-   $('.slick-slider').slick('slickGoTo', slideno - 1);
- });
+$('a[data-slide]').click(function(e) {
+  e.preventDefault();
+  var slideno = $(this).data('slide');
+  $('.slick-slider').slick('slickGoTo', slideno - 1);
+});
 
- $('.menu').hover('li').hover(function(e){
-   var index = $(this).data();
-   var slideno = $(this).data('slide');
-   $('.slick-slider').slick('slickGoTo', slideno +1);
- })
+$('.menu').hover('li').hover(function(e) {
+  var index = $(this).data();
+  var slideno = $(this).data('slide');
+  $('.slick-slider').slick('slickGoTo', slideno + 1);
+})
 
-$('.js-slick-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
-    $('.menu li:nth-child('+(currentSlide - 1)+')').removeClass('slide-active');
-  $('.menu li:nth-child(' + (currentSlide)+ ')').addClass('slide-active');
+$('.js-slick-slider').on('afterChange', function(event, slick, currentSlide, nextSlide) {
+  $('.menu li:nth-child(' + (currentSlide - 1) + ')').removeClass('slide-active');
+  $('.menu li:nth-child(' + (currentSlide) + ')').addClass('slide-active');
 });
 
 $(".js-slick-slider").on("beforeChange", function() {
 
-   $('.home-image-tab').removeClass('animated fadeInLeft').hide();
-   setTimeout(() => {
-     $('.home-image-tab').addClass('animated fadeInLeft').show();
+  $('.home-image-tab').removeClass('animated fadeInLeft').hide();
+  setTimeout(() => {
+    $('.home-image-tab').addClass('animated fadeInLeft').show();
 
-   }, 1000);
+  }, 1000);
 
- })
+})
 
-$('.js-slick-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
-  if(currentSlide === 0){
+$('.js-slick-slider').on('afterChange', function(event, slick, currentSlide, nextSlide) {
+  if (currentSlide === 0) {
     $('ul li:nth-child(4)').removeClass('slide-active');
   }
 });
 
- $('a.previous').click(function(e) {
-$('.js-slick-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
-    $('li:nth-child('+(currentSlide + 1)+')').removeClass('slide-active');
-  $('ul li:nth-child(' + (currentSlide)+')').addClass('slide-active');
-});
+$('a.previous').click(function(e) {
+  $('.js-slick-slider').on('afterChange', function(event, slick, currentSlide, nextSlide) {
+    $('li:nth-child(' + (currentSlide + 1) + ')').removeClass('slide-active');
+    $('ul li:nth-child(' + (currentSlide) + ')').addClass('slide-active');
+  });
 });
