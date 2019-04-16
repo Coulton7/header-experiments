e8c37fa48af6363d03bb3d392f30c2c5c6280220
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-04-16 15:58:23 +0100

    trying another variation of code for the slide number

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ $('a.previous').click(function(e) {
 $(".menu li").hover(function(e){
        e.preventDefault();
        var menuItem = $(this).index('.menu li');
-       var slideno = $('.menu li:nth-child('+ (menuItem)+ ')').eq('a[data-slide]');
-       $('.slick-slider').slick('slickGoTo', slideno);
+       var slideno = $(this).data('slide'+(menuItem));
+       $('.slick-slider').slick('slickGoTo', slideno+1);
      });
    })(jQuery);
