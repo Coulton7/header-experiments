35c33ca4a5e3c91eb73adce67c66492f880639b5
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-04-16 14:43:46 +0100

    giving the menu item value for the slideno to taregt

--- index.js
+++ index.js
@@ -53,8 +53,8 @@ $('a.previous').click(function(e) {
 
 $(".menu li").hover(function(e){
        e.preventDefault();
-       var currentSlide = $('.slick-slider').slick('slickCurrentSlide');
-       var slideno = $('.slick-slider').data('slide')=== $('.menu li:nth-child('+ (currentSlide)+ ')');
+       var menuItem = $(this).data('.menu li');
+       var slideno = $('.menu li:nth-child('+ (menuItem)+ ')');
        $('.slick-slider').slick('slickGoTo', slideno);
      });
    })(jQuery);
