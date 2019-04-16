9d767cc0781b324fa05180dadc5abeafcf58c8da
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-04-16 16:14:25 +0100

    trying.index value instead

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ $('a.previous').click(function(e) {
 $(".menu li").hover(function(e){
        e.preventDefault();
        var menuItem = $(this).index('.menu li');
-       var slideno = $('slide').data('menu li nth:child('+ menuItem + ')');
+       var slideno = $('slide').index('menu li nth:child('+ menuItem + ')');
        console.log(slideno)
        $('.slick-slider').slick('slickGoTo', slideno + 1);
      });
