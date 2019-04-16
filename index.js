a8341d0cbf532165e4d6cb8f08c2d9e61360835c
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-04-16 15:33:06 +0100

    adjusting the script to see if it goes to the correct slide

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ $('a.previous').click(function(e) {
 $(".menu li").hover(function(e){
        e.preventDefault();
        var menuItem = $(this).index('.menu li');
-       var slideno = $('.menu li:nth-child('+ (menuItem)+ ')');
+       var slideno = $('.menu li:nth-child('+ (menuItem)+ ')').data('.slide');
        console.log(slideno)
        $('.slick-slider').slick('slickGoTo', slideno);
      });
