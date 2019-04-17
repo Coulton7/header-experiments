a6e29274660947bc2cca659c30aa7e27a04a51d4
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-04-17 09:45:34 +0100

    Forgot the speech marks

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ $('a.previous').click(function(e) {
 $(".menu li").hover(function(e){
        e.preventDefault();
        var menuItem = $(this).index() + 1;
-       var slideFigure = $(menu li nth:child('+ menuItem + '));
+       var slideFigure = $('menu li nth:child('+ menuItem + ')');
        var slideno = $(slideFigure).eq('slide');
        console.log(slideno)
        $('.slick-slider').slick('slickGoTo', slideno + 1);
