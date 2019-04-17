5b678bae8f94c6a5f89dd129ea2853547f2cce33
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-04-17 12:33:26 +0100

    adding an if statement o only do the li on so much

--- index.js
+++ index.js
@@ -52,8 +52,11 @@ $('a.previous').click(function(e) {
 });
 
 $(".menu li").hover(function(e){
+  var $this = $(this);
+  if($this.index() === 0|| $this.is(':nth-child(1)')|| $this.is(':nth-child(2)')|| $this.is(':nth-child(3)')){
        e.preventDefault();
        var menuItem = $('li').index(this);
        $('.slick-slider').slick('slickGoTo', menuItem + 1);
+     }
      });
    })(jQuery);
