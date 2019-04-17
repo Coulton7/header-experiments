23010d79231e6176223b8c3562c6393ae1590d4a
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-04-17 12:06:04 +0100

    Trying to add the .class name to it to see if it returns a correct value

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ $('a.previous').click(function(e) {
 
 $(".menu li").hover(function(e){
        e.preventDefault();
-       var menuItem = $(this).index('menu');
+       var menuItem = $(this).index('.menu');
        var slideFigure = $('.menu li :eq('+ menuItem + ')').html();
        var slideno = $(menuItem).eq('slide');
        console.log(slideno)
